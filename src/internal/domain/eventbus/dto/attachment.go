7d66ead05866bd8935c2fd8bd0ca3d657ed06ce0
Gamafoot <gamafootsas@gmail.com>
2025-12-04 13:44:52 +0900

    fix json field name

--- src/internal/domain/eventbus/dto/attachment.go
+++ src/internal/domain/eventbus/dto/attachment.go
@@ -2,7 +2,7 @@ package dto
 
 type Attachment struct {
 	ID       uint   `json:"id"`
-	FilePath string `json:"file_paths"`
+	FilePath string `json:"file_path"`
 	FileSize int64  `json:"file_size"`
 	MimeType string `json:"mime_type"`
 }
